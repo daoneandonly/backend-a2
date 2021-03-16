2e529bb422fb6eddbb1a03d9bea0f4d8fe071da0
Steven <stevenawhitton@gmail.com>
2021-03-16 19:17:27 +0100

    Add express/nodemon and add basic server

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
-console.log("Hello World 👋")
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
+
+// Resolve GET request
+app.get('/', (req, res) => {
+  res.send('Hello world!! 👋')
+});
+
+// Listen to port 3000
+app.listen(port, () => {
+  console.log(`App.js starting at http://localhost:${port}`);
+});
