bdc0c38766480641e8153b7f158fe850657b2d9c
maxwieringa <maxwieringa01@gmail.com>
2021-03-24 19:46:18 +0100

    Added multer and profile functions

--- app.js
+++ app.js
@@ -1,9 +1,15 @@
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
+const multer = require('multer')
+const upload = multer({
+  dest: 'static/img/'
+})
 
 app.set('view engine', 'ejs');
 app.use(express.static('static'));
+app.get('/add', profileform)
+app.post('/add', upload.single('foto'), add)
 
 // Resolve GET request
 app.get('/', (req, res) => {
@@ -25,19 +31,41 @@ app.get('/bucketlistResultaat', showBucketlistResultaat);
 
 //function render bucketlistOvezicht page
 function showBucketlistOverzicht(req, res) {
-    res.render('pages/bucketlist/bucketlistOverzicht');
-  };
+  res.render('pages/bucketlist/bucketlistOverzicht');
+};
 
-  //function render bucketlistResultaat page
+//function render bucketlistResultaat page
 function showBucketlistResultaat(req, res) {
-    res.render('pages/bucketlist/bucketlistResultaat');
-  };
+  res.render('pages/bucketlist/bucketlistResultaat');
+};
+
+function profileform(req, res) {
+  res.render('add.ejs')
+}
+
+function add(req, res, next) {
+  db.collection('profiles').insertOne({
+    naam: req.body.naam,
+    foto: req.file ? req.file.filename : null,
+    leeftijd: req.body.leeftijd,
+    bio: req.body.bio
+  }, done)
+
+  function done(err, data) {
+    if (err) {
+      next(err)
+    } else {
+      res.redirect('/' + data.insertedId)
+    }
+  }
+}
+
 
 // If there is no page found give an error page as page
 app.get('*', (req, res) => {
   res.status(404).render('pages/404', {
-      url: req.url,
-      title: 'Error 404',
+    url: req.url,
+    title: 'Error 404',
   })
 });
 
