23677d006ad6418beb110e734667d2879e264e99
Jeroen777 <jeroenbesselink98@gmail.com>
2021-03-25 10:35:13 +0100

    mongoose npm installed

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
 const multer = require('multer');
+const mongoose = require('mongoose');
 const upload = multer({
   dest: 'static/img/'
 });
