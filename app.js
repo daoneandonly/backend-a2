750687c75f935c8fa288a9c01507fe19d325b8e5
Steven <stevenawhitton@gmail.com>
2021-03-28 17:02:45 +0200

    Fix minor changes

--- app.js
+++ app.js
@@ -6,16 +6,15 @@ const upload = multer({
   dest: 'static/img/'
 });
 
+// Mongoose
+const mongoose = require('mongoose')
+
 // dotenv
 const dotenv = require('dotenv');
 dotenv.config();
 
 app.set('view engine', 'ejs');
 app.use(express.static('static'));
-
-// Mongoose
-const mongo = require('mongodb')
-const mongoose = require('mongoose')
 
 const db = mongoose.connection
 
@@ -72,62 +71,62 @@ app.post('/registerUsers', (req, res) => {
   }
 })
 
-  app.get('/add', profileForm);
-  app.post('/add', upload.single('photo'), add);
-  
-  //bucketlist
-  app.get('/bucketlist', showBucketlistOverview);
-  app.get('/bucketlistResults', showBucketlistResults);
+app.get('/add', profileForm);
+app.post('/add', upload.single('photo'), add);
 
-  app.post('/bucketlistResults', showBucketlistResults);
-  
-  
-  // If there is no page found give an error page as page
-  app.get('*', (req, res) => {
-    res.status(404).render('pages/404', {
-      url: req.url,
-      title: 'Error 404',
-    })
+//bucketlist
+app.get('/bucketlist', showBucketlistOverview);
+app.get('/bucketlistResults', showBucketlistResults);
+
+app.post('/bucketlistResults', showBucketlistResults);
+
+
+// If there is no page found give an error page as page
+app.get('*', (req, res) => {
+  res.status(404).render('pages/404', {
+    url: req.url,
+    title: 'Error 404',
+  })
+});
+
+  //function render bucketlistOverview page
+function showBucketlistOverview(req, res) {
+  res.render('pages/bucketlist/bucketlistOverview', {
+    title: 'bucketlist'
   });
-  
-    //function render bucketlistOverview page
-  function showBucketlistOverview(req, res) {
-    res.render('pages/bucketlist/bucketlistOverview', {
-      title: 'bucketlist'
-    });
-  };
+};
 
-  //function render bucketlistResultaat page
-  function showBucketlistResults(req, res) {
-    res.render('pages/bucketlist/bucketlistResults', {
-      title: 'bucketlistoverview'
-    }, {
-      interestView: data
-    });
-  };
+//function render bucketlistResultaat page
+function showBucketlistResults(req, res) {
+  res.render('pages/bucketlist/bucketlistResults', {
+    title: 'bucketlistoverview'
+  }, {
+    interestView: data
+  });
+};
 
-  function profileForm(req, res) {
-    res.render('add.ejs')
-  }
+function profileForm(req, res) {
+  res.render('add.ejs')
+}
 
-  function add(req, res, next) {
-    db.collection('profiles').insertOne({
-      name: req.body.name,
-      photo: req.file ? req.file.filename : null,
-      age: req.body.age,
-      bio: req.body.bio
-    }, done);
+function add(req, res, next) {
+  db.collection('profiles').insertOne({
+    name: req.body.name,
+    photo: req.file ? req.file.filename : null,
+    age: req.body.age,
+    bio: req.body.bio
+  }, done);
 
-    function done(err, data) {
-      if (err) {
-        next(err)
-      } else {
-        res.redirect('/' + data.insertedId)
-      }
+  function done(err, data) {
+    if (err) {
+      next(err)
+    } else {
+      res.redirect('/' + data.insertedId)
     }
   }
-  
-  // Listen to port 3000
-  app.listen(port, () => {
-    console.log(`App.js starting at http://localhost:${port}`);
-  });
+}
+
+// Listen to port 3000
+app.listen(port, () => {
+  console.log(`App.js starting at http://localhost:${port}`);
+});
