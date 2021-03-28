5033e4164e114c43d471d4ec8d8811a3ab5e1b50
Pepijn Kok <59015908+pepijnkok@users.noreply.github.com>
2021-03-28 15:03:20 +0200

    Revert "Register branch update"

--- app.js
+++ app.js
@@ -10,28 +10,25 @@ const upload = multer({
 const dotenv = require('dotenv');
 dotenv.config();
 
+
 app.set('view engine', 'ejs');
 app.use(express.static('static'));
 
-// Mongoose
-const mongo = require('mongodb')
-const mongoose = require('mongoose')
 
-const db = mongoose.connection
+// Mongodb
+const MongoClient = require('mongodb').MongoClient;
+const uri = process.env.DB_URI;
+const client = new MongoClient(uri,{ useUnifiedTopology: true });
 
-// Connect mongoose with the database
-mongoose.connect(process.env.DB_URI, {
-  useNewUrlParser: true, 
-  useUnifiedTopology: true
-})
-
-db.on('connected', () => { 
-  console.log('Mongoose connected')
-})
-
-// ejs
 app.set('view engine', 'ejs');
 app.use(express.static('static'));
+
+client.connect()
+.then(async client => {
+  let data = []
+  const db = client.db("dateApp");
+
+  data = await db.collection("users").find({}).toArray();
   
   // Resolve GET request
   app.get('/', (req, res) => {
@@ -45,32 +42,6 @@ app.use(express.static('static'));
       title: 'register'
     })
   });
-
-  // Telling app to take the forms and acces them inside of the request variable inside of the post method
-app.use(express.urlencoded({ extended: false }))
-
-// Create users collection with schema
-const Users = mongoose.model('Users',{name: String,email:String,password:String});
-
-app.post('/registerUsers', (req, res) => {
-   
-  try {
-     const newUsers  = new Users({
-        name: req.body.name,
-        email: req.body.email,
-        password: req.body.password
-     })
-     newUsers.save().then(() =>{
-        console.log('Added Users');
-        res.redirect('/login')
-        return;
-        
-  })
-     
-  } catch (error) {
-     console.log(error);
-  }
-})
 
   app.get('/add', profileForm);
   app.post('/add', upload.single('photo'), add);
@@ -89,6 +60,7 @@ app.post('/registerUsers', (req, res) => {
       title: 'Error 404',
     })
   });
+  
   
     //function render bucketlistOverview page
   function showBucketlistOverview(req, res) {
@@ -131,3 +103,5 @@ app.post('/registerUsers', (req, res) => {
   app.listen(port, () => {
     console.log(`App.js starting at http://localhost:${port}`);
   });
+})
+.catch(console.error);
